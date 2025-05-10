a579aaef3add6275c6d7512ec23c910f7c417920
george-asiedu <asiedug41@gmail.com>
2025-05-10 16:58:06 +0000

    loading state ui changed to use a loading indicator

--- src/app/(ims)/loading.tsx
+++ src/app/(ims)/loading.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function loading() {
-  return <div>loading......</div>;
+  return (
+    <div className="flex w-full flex-col items-center justify-center gap-4">
+      <div className="flex h-20 w-20 animate-spin items-center justify-center rounded-full border-4 border-transparent border-t-blue-400 text-4xl text-blue-400">
+        <div className="flex h-16 w-16 animate-spin items-center justify-center rounded-full border-4 border-transparent border-t-red-400 text-2xl text-red-400"></div>
+      </div>
+    </div>
+  );
 }
