ea65520ab8824e20251a720a2bd4d82d56eebf66
george-asiedu <asiedug41@gmail.com>
2025-05-13 13:41:55 +0000

    optimize inventory component added

--- src/app/landing-page/page.tsx
+++ src/app/landing-page/page.tsx
@@ -1,9 +1,11 @@
 import Home from "@/app/landing-page/home";
+import OptimizeInventory from "@/app/landing-page/optimize";
 
 export default function LandingPage() {
   return (
     <div className="flex h-auto w-full flex-col">
       <Home />
+      <OptimizeInventory />
     </div>
   );
 }
