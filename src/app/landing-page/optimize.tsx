06de50cde46e5c9070f4be01850acc663d45e365
george-asiedu <asiedug41@gmail.com>
2025-05-14 10:02:45 +0000

    break tags added

--- src/app/landing-page/optimize.tsx
+++ src/app/landing-page/optimize.tsx
@@ -6,7 +6,7 @@ export default function OptimizeInventory() {
     <section className="flex h-auto w-full flex-col gap-8 bg-[#111111] px-6 py-14 md:px-8 lg:h-screen lg:gap-20 lg:px-[120px] lg:pt-20">
       <div className="grid w-full grid-cols-1 gap-4 text-white lg:grid-cols-2 lg:gap-16">
         <h3 className="text-3xl font-bold tracking-wide md:text-[42px]">
-          Optimizing Inventory for Healthcare Providers
+          Optimizing Inventory for <br /> Healthcare Providers
         </h3>
         <p className="tracking-wide opacity-60">
           Whether you're a hospital or a local pharmacy, IMS streamlines stock
