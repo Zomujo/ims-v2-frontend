f3af0ebac116d35cd060653babfbb6fdd301691b
george-asiedu <asiedug41@gmail.com>
2025-05-14 10:03:09 +0000

    features component added to landing page

--- src/app/landing-page/page.tsx
+++ src/app/landing-page/page.tsx
@@ -1,11 +1,13 @@
 import Home from "@/app/landing-page/home";
 import OptimizeInventory from "@/app/landing-page/optimize";
+import Features from "@/app/landing-page/features";
 
 export default function LandingPage() {
   return (
     <div className="flex h-auto w-full flex-col">
       <Home />
       <OptimizeInventory />
+      <Features />
     </div>
   );
 }
