758bcf05e4e99ea1c7244c18bf13cf2549aebdc7
= <abubakaribilal99@gmail.com>
2025-05-17 23:23:53 +0000

    feat: add date range filter for item batches and improve filter form integration

--- src/features/layout/search-with-filter/fiter-form.tsx
+++ src/features/layout/search-with-filter/fiter-form.tsx
@@ -1,17 +1,24 @@
 "use client";
 import { PAGE_ROUTES } from "@/lib/constant";
-import { usePathname } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 import React from "react";
 import { ItemsFilters } from "@features/items/items-filters";
+import { ItemsBatchesFilters } from "@features/items/items-batches-filters";
 
 export default function FilterForms() {
   const pathName = usePathname();
+  const params = useParams();
+  const itemId = params.slugs[0];
+
+  const renderFilterForm = () => {
+    return {
+      [PAGE_ROUTES.SALES.VIEW]: <div>Sales</div>,
+      [PAGE_ROUTES.ITEMS.VIEW]: <ItemsFilters />,
+      [PAGE_ROUTES.ITEMS.BATCHES.replace(":itemId", itemId)]: (
+        <ItemsBatchesFilters />
+      ),
+    };
+  };
+
   return renderFilterForm()[pathName] ?? <div>FilterForms</div>;
 }
-
-const renderFilterForm = () => {
-  return {
-    [PAGE_ROUTES.SALES.VIEW]: <div>Sales</div>,
-    [PAGE_ROUTES.ITEMS.VIEW]: <ItemsFilters />,
-  };
-};
