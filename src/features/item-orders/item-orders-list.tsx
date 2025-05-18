bad12855aaf55d5e7b5ff27c498220e9277d9351
= <abubakaribilal99@gmail.com>
2025-05-18 11:21:36 +0000

    feat: enhance item orders management with delivery details and order status filter

--- src/features/item-orders/item-orders-list.tsx
+++ src/features/item-orders/item-orders-list.tsx
@@ -4,7 +4,9 @@ import {
   changeItemOrderState,
   createItemOrder,
   deleteItemOrder,
+  getItemOrder,
   getItemOrders,
+  updateItemOrder,
 } from "../shared/actions/item-orders.actions";
 import CrudPage from "../shared/components/crud-page";
 import { ImsButton } from "../shared/components/ims-button";
@@ -22,6 +24,8 @@ import { orderFormSchema } from "./item-orders.schema";
 import { z } from "zod";
 import useImsSearchParams from "../shared/hooks/use-ims-search-params";
 import { UI_STATE } from "@/lib/constant";
+import { useEffect, useState } from "react";
+import LoadingOverlay from "@features/ui/loadingOverlay";
 
 type ItemOrdersListProps = {
   items: IdData[];
@@ -32,7 +36,7 @@ export default function ItemOrdersList({
   items,
   suppliers,
 }: Readonly<ItemOrdersListProps>) {
-  const { data } = useFetchData({
+  const { data, loading, refetch } = useFetchData({
     fetchFn: getItemOrders,
   });
   const itemOrders = data?.rows ?? [];
@@ -61,7 +65,7 @@ export default function ItemOrdersList({
     const res = changeItemOrderState(id, { status });
     handleRequestState({ res, loadingMsg: "Changing item order state..." });
     res.then(() => {
-      console.log("Item order state changed successfully");
+      refetch();
     });
   };
   const handleCurrentState = (status: string) => {
@@ -77,6 +81,7 @@ export default function ItemOrdersList({
       <CrudPage
         moduleName="item categories"
         data={itemOrders}
+        isLoading={loading}
         modalAction={handleDelete}
         handleRemoveQueryparam={handleRemoveQueryparam}
         currentDataDisplayName={getData(CRUDACTION.DELETE)?.orderNumber ?? ""}
@@ -91,12 +96,13 @@ export default function ItemOrdersList({
               icon: "lucide:edit-2",
               action: () => handleEditBtnClicked(item),
             },
-            {
-              label: "print PDF",
-              icon: "solar:printer-2-outline",
-              action: () => handleDeleteBtnClicked(item),
-            },
-            {
+            // TODO: Will probably be added but at the moment, no functionality for this
+            // {
+            //   label: "print PDF",
+            //   icon: "solar:printer-2-outline",
+            //   action: () => handleDeleteBtnClicked(item),
+            // },
+            item.status !== ItemOrderStatus.RECEIVED && {
               label: handleCurrentState(item.status) ?? "",
               action: () =>
                 handleItemOrderState(
@@ -105,7 +111,7 @@ export default function ItemOrdersList({
                 ),
               icon: "hugeicons:view",
             },
-            {
+            item.status !== ItemOrderStatus.RECEIVED && {
               label: "delete",
               icon: "lucide:trash-2",
               type: "destructive",
@@ -121,14 +127,24 @@ export default function ItemOrdersList({
         }
       >
         <ItemOrdersProvider value={{ items, suppliers }}>
-          <ItemOrdersForm />
+          <ItemOrdersForm
+            itemOrderId={getId(CRUDACTION.EDIT)}
+            isEditMode={isEditMode}
+          />
         </ItemOrdersProvider>
       </CrudPage>
     </ScrollArea>
   );
 }
 
-export function ItemOrdersForm() {
+export function ItemOrdersForm({
+  isEditMode,
+  itemOrderId,
+}: Readonly<{
+  isEditMode?: boolean;
+  itemOrderId?: string;
+}>) {
+  const [isLoading, setIsLoading] = useState(false);
   const { removeSearchParams } = useImsSearchParams();
   const form = useHookForm({
     resolver: orderFormSchema,
@@ -146,39 +162,62 @@ export function ItemOrdersForm() {
 
   const handleSubmit = async (data: unknown) => {
     const orderData = data as z.infer<typeof orderFormSchema>;
-    const res = createItemOrder({
-      ...orderData,
-      status: ItemOrderStatus.DRAFT,
-    });
+    const res = isEditMode
+      ? updateItemOrder(itemOrderId, orderData)
+      : createItemOrder({
+          ...orderData,
+          status: ItemOrderStatus.DRAFT,
+        });
     handleRequestState({
       res,
-      loadingMsg: "Creating item order...",
+      loadingMsg: isEditMode
+        ? "Updating item order..."
+        : "Creating item order...",
     });
     res.then(() => {
       form.reset();
       removeSearchParams(UI_STATE);
     });
-
-    // Add your form submission logic here (e.g., API call to create order)
   };
 
-  return (
-    <ImsForm
-      form={form}
-      handleAuthSubmit={handleSubmit}
-      RenderInputs={<ItemOrdersInputs control={form.control} />}
-      RenderActions={
-        <ImsButton
-          type="submit"
-          variant="imsPrimary"
-          isLoading={form.formState.isSubmitting}
-          isLoadingLabel="Saving..." // Example loading label
-        >
-          Save
-        </ImsButton>
+  useEffect(() => {
+    if (!isEditMode) return;
+    const fetchItemOrder = async () => {
+      setIsLoading(true);
+      const { data } = await getItemOrder(itemOrderId ?? "");
+      if (data) {
+        const { expectedDeliveryDate } = data;
+        form.reset({
+          ...data,
+          expectedDeliveryDate: expectedDeliveryDate.split("T")[0],
+        });
+        setIsLoading(false);
       }
-      className="overflow-y-auto [&>*]:px-4"
-      inputSectionClassName="overflow-y-auto"
-    />
+    };
+    void fetchItemOrder();
+  }, []);
+
+  return (
+    <>
+      {isLoading && <LoadingOverlay />}
+      <ImsForm
+        form={form}
+        handleAuthSubmit={handleSubmit}
+        RenderInputs={<ItemOrdersInputs control={form.control} />}
+        RenderActions={
+          <ImsButton
+            type="submit"
+            variant="imsPrimary"
+            disabled={form.formState.isSubmitting}
+            isLoading={form.formState.isSubmitting}
+            isLoadingLabel="Saving..."
+          >
+            Save
+          </ImsButton>
+        }
+        className="overflow-y-auto [&>*]:px-4"
+        inputSectionClassName="overflow-y-auto"
+      />
+    </>
   );
 }
