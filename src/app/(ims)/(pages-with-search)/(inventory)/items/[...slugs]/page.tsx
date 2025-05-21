4487874f0e8b996e57e44b97285d55884da5dcb1
= <abubakaribilal99@gmail.com>
2025-05-21 06:42:10 +0000

    feat: add server-side permission check for item suppliers in ItemBatches

--- src/app/(ims)/(pages-with-search)/(inventory)/items/[...slugs]/page.tsx
+++ src/app/(ims)/(pages-with-search)/(inventory)/items/[...slugs]/page.tsx
@@ -3,18 +3,23 @@ import { getSuppliersNoPaginate } from "@/features/shared/actions/supplier.actio
 import { PAGE_ROUTES } from "@/lib/constant";
 import { redirect, RedirectType } from "next/navigation";
 import { getItem } from "@features/shared/actions/items.actions";
+import { checkServerPermission } from "@/lib/providers/server-permission-provider";
+import { PermissionModules } from "@features/shared/types/auth-action.types";
 
 export default async function ItemBatches({
   params,
 }: {
   params: Promise<{ slugs: string[] }>;
 }) {
+  const hasPermission = await checkServerPermission(
+    PermissionModules.SUPPLIERS,
+  );
   const { slugs } = await params;
   const [itemId, pageRoute] = slugs;
   if ((!itemId || !pageRoute) && pageRoute !== PAGE_ROUTES.ITEM_BATCHES) {
     redirect(PAGE_ROUTES.ITEMS.VIEW, RedirectType.replace);
   }
-  const suppliers = await getSuppliersNoPaginate();
+  const suppliers = hasPermission ? await getSuppliersNoPaginate() : [];
   const { data } = await getItem(itemId);
 
   return (
