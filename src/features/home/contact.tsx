5574a3e7242303d2df2e605675b8fd5cdae5bbb0
= <abubakaribilal99@gmail.com>
2025-06-12 13:47:25 +0000

    fix: adjust alignment and padding in contact section for better responsiveness

--- src/features/home/contact.tsx
+++ src/features/home/contact.tsx
@@ -5,20 +5,20 @@ export default function Contact() {
   return (
     <section id="contact" className="relative flex w-full flex-col bg-white">
       <div className="relative grid h-auto w-full grid-cols-1 gap-0 lg:h-[745px] lg:grid-cols-2">
-        <div className="flex w-full flex-col gap-4 bg-gradient-to-b from-[#2A3FAC] via-[#2A3FAC] to-[#e5e4ed] px-6 py-20 md:px-8 lg:h-full lg:px-[120px] lg:py-32">
-          <h6 className="font-bold tracking-wide text-white uppercase">
+        <div className="flex w-full flex-col gap-4 bg-gradient-to-b from-[#2A3FAC] via-[#2A3FAC] to-[#e5e4ed] px-6 py-20 md:px-8 lg:h-full lg:py-32 xl:px-[150px]">
+          <h6 className="font-bold tracking-wide text-white uppercase xl:px-8">
             Do you run a pharmacy? get in touch
           </h6>
-          <h4 className="text-4xl font-bold tracking-wide text-white md:text-[42px]">
+          <h4 className="text-4xl font-bold tracking-wide text-white md:text-[42px] xl:px-8">
             Empower <span className="text-[#FE9748]">Rural</span> <br />{" "}
             Pharmacies with IMS
           </h4>
-          <p className="my-6 tracking-wide text-white opacity-60">
+          <p className="my-6 tracking-wide text-white opacity-60 xl:px-8">
             Effortlessly manage inventory, streamline reordering, reduce waste,
             and boost operational efficiency—all within a single, easy-to-use
             app designed specifically to meet the needs of modern pharmacies.
           </p>
-          <div className="flex w-fit flex-col gap-3 lg:flex-row lg:items-center">
+          <div className="flex w-fit flex-col gap-3 lg:flex-row lg:items-center xl:px-8">
             <button className="z-30 cursor-pointer rounded-lg bg-white px-6 py-2 font-medium text-[#111] hover:bg-gray-100">
               Book a demo
             </button>
@@ -27,7 +27,7 @@ export default function Contact() {
             </button>
           </div>
         </div>
-        <div className="flex w-full flex-col gap-4 bg-gradient-to-b from-[#CCE0FF] to-[#e5e4ed] px-6 py-20 md:px-8 lg:h-full lg:px-[120px] lg:py-32">
+        <div className="flex w-full flex-col gap-4 bg-gradient-to-b from-[#CCE0FF] to-[#e5e4ed] px-6 py-20 md:px-8 lg:h-full lg:py-32 xl:px-[150px]">
           <h6 className="font-bold tracking-wide text-[#111]">
             ACCESS YOUR IMS INVENTORY
           </h6>
