4ab8d5b88da365871c68761a2f2722e22e701de0
= <abubakaribilal99@gmail.com>
2025-07-11 20:51:03 +0000

    feat: implement offline page and enhance caching configuration

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
+  fallbacks: {
+    document: "/~offline",
+  },
+  cacheStartUrl: true,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  extendDefaultRuntimeCaching: true,
 });
 
 const nextConfig = {
