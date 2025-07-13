ef95dbaa17d25d6cfaeb6ae0c0346a4536ee3bf0
= <abubakaribilal99@gmail.com>
2025-07-13 12:53:41 +0000

    feat: enhance PWA configuration with precache manifest and workbox options

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 // eslint-disable-next-line @typescript-eslint/no-require-imports
+const precacheManifest = require("./precache-manifest");
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
   fallbacks: {
@@ -16,6 +19,24 @@ const nextConfig = {
   eslint: {
     dirs: ["src"],
     ignoreDuringBuilds: true,
+  },
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    dynamicStartUrl: false,
+    additionalManifestEntries: precacheManifest.map((url: never) => ({
+      url,
+      revision: null,
+    })),
+    workboxOptions: {
+      exclude: [
+        /\.map$/,
+        /\/_next\/static\/.*\.woff2$/,
+        /^manifest.*\.json$/,
+        "/some-large-lib.js",
+      ],
+    },
   },
 } satisfies NextConfig;
 
