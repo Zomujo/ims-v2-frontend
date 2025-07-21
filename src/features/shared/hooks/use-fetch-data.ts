6a917cff29924bf4082cfdd777dc9b0f0958adff
= <abubakaribilal99@gmail.com>
2025-07-21 07:58:08 +0000

    fix: optimize data fetching by tracking previous search parameters

--- src/features/shared/hooks/use-fetch-data.ts
+++ src/features/shared/hooks/use-fetch-data.ts
@@ -1,5 +1,5 @@
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { GenerateQueryParams } from "../types/utitls.types";
 
 type FetchDataProps<T> = {
@@ -30,6 +30,7 @@ export default function useFetchData<T>({
   const [error, setError] = useState<Error | null>(null);
   const [data, setData] = useState<T | null>(null);
   const searchParams = useSearchParams();
+  const prevSearchParams = useRef<string>("");
 
   const fetchData = async () => {
     const queryParams = Object.fromEntries(searchParams.entries());
@@ -65,6 +66,28 @@ export default function useFetchData<T>({
 
   useEffect(() => {
     if (!exercuteOnMount) return;
+
+    const newSearchParamsString = searchParams.toString();
+    const oldSearchParamsString = prevSearchParams.current;
+    prevSearchParams.current = newSearchParamsString;
+
+    const state = searchParams.get("state");
+
+    if (
+      (state === "create" || state === "edit") &&
+      oldSearchParamsString !== undefined
+    ) {
+      const newParamsCopy = new URLSearchParams(newSearchParamsString);
+      const oldParamsCopy = new URLSearchParams(oldSearchParamsString);
+
+      newParamsCopy.delete("state");
+      oldParamsCopy.delete("state");
+
+      if (newParamsCopy.toString() === oldParamsCopy.toString()) {
+        return;
+      }
+    }
+
     void fetchData();
   }, [searchParams, ...deps]);
 
