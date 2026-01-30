e39ccded321ecf122f6f8eef0ee22fce7ab0baaa
Bilal Abubakari <abubakaribilal99@gmail.com>
2026-01-30 09:39:22 +0000

    fix: comment out service worker configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,13 +7,13 @@ import withSerwist from "@serwist/next";
 
 export default async (phase: string) => {
   const nextConfig: NextConfig = {};
-  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
-    return withSerwist({
-      swSrc: "public/service-worker/sw.ts",
-      swDest: "public/sw.js",
-      reloadOnOnline: true,
-    })(nextConfig);
-  }
+  // if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+  //   return withSerwist({
+  //     swSrc: "public/service-worker/sw.ts",
+  //     swDest: "public/sw.js",
+  //     reloadOnOnline: true,
+  //   })(nextConfig);
+  // }
 
   return nextConfig;
 };
